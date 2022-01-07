644ba42ce15abeb6c7b4ceb85ba10975e181aa73
luukaszp <luukaszpoterala@wp.pl>
2022-01-07 10:56:43 +0100

    role_user pivot + auth

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::prefix('categories')->group( function () {
     Route::get('/create', [CategoryController::class, 'create']);
     Route::post('/store', [CategoryController::class, 'store']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
